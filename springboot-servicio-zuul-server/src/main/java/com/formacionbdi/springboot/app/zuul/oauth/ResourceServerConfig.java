0115e7dbd0be046bd18a0be60b93961d20bca089
yanpieer-romero-unmsm <yanpieer.romero@unmsm.edu.pe>
2022-11-23 01:32:39 -0500

    Configuration oauth, token and cors v2

--- springboot-servicio-zuul-server/src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ springboot-servicio-zuul-server/src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,15 +1,11 @@
 package com.formacionbdi.springboot.app.zuul.oauth;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -20,6 +16,11 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.GET;
 
 @RefreshScope
 @Configuration
@@ -38,9 +39,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		http.authorizeRequests()
 				.antMatchers("/api/security/oauth/**")
 				.permitAll()
-				.antMatchers(HttpMethod.GET, "/api/products", "/api/items", "/api/users/users")
+				.antMatchers(GET, "/api/products", "/api/items", "/api/users/users")
 				.permitAll()
-				.antMatchers(HttpMethod.GET, "/api/products/{id}", "/api/items/{id}/amount/{amount}", "/api/users/users/{id}")
+				.antMatchers(GET, "/api/products/{id}", "/api/items/{id}/amount/{amount}", "/api/users/users/{id}")
 				.hasAnyRole("ADMIN", "USER")
 				.antMatchers("/api/products/**", "/api/items/**", "/api/users/**")
 				.hasRole("ADMIN")
@@ -49,11 +50,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 				.and()
 				.cors()
 				.configurationSource(corsConfigurationSource());
-		/*
-		.antMatchers(HttpMethod.POST, "/api/productos", "/api/items", "/api/usuarios/usuarios").hasRole("ADMIN")
-		.antMatchers(HttpMethod.PUT, "/api/productos/{id}", "/api/items/{id}", "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
-		.antMatchers(HttpMethod.DELETE, "/api/productos/{id}", "/api/items/{id}", "/api/usuarios/usuarios/{id}").hasRole("ADMIN");
-		*/
 	}
 	
 	@Bean
@@ -72,7 +68,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Bean
 	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
 		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return bean;
 	}
